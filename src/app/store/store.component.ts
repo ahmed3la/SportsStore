366f0ca62b88f7d62c300440546309a00b84f665
ahmed <ahmed3la@gmail.com>
2021-03-27 00:43:16 +0200

    Adding Product Pagination

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -11,9 +11,14 @@ export class StoreComponent {
   constructor(private repository: ProductRepository) { }
 
   public selectedCategory = null;
+  public productsPerPage = 4;
+  public selectedPage = 1;
+
 
   get products(): Product[] {
-    return this.repository.getProducts(this.selectedCategory);
+    let pageIndex = (this.selectedPage - 1) * this.productsPerPage
+    return this.repository.getProducts(this.selectedCategory)
+      .slice(pageIndex, pageIndex + this.productsPerPage);
   }
   get categories(): string[] {
     return this.repository.getCategories();
@@ -23,5 +28,19 @@ export class StoreComponent {
     this.selectedCategory = newCategory;
   }
 
+  changePage(newPage: number) {
+    this.selectedPage = newPage;
+  }
+
+  changePageSize(newSize: number) {
+    this.productsPerPage = Number(newSize);
+    this.changePage(1);
+  }
+
+  get pageNumbers(): number[] {
+    return Array(Math.ceil(this.repository
+      .getProducts(this.selectedCategory).length / this.productsPerPage))
+      .fill(0).map((x, i) => i + 1);
+  }
 }
 
