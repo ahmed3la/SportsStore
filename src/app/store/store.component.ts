1e146128017df632cee4fe5e213c2b1cdcbb0f29
ahmed <ahmed3la@gmail.com>
2021-03-27 00:28:49 +0200

    Adding Category Selection

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -10,11 +10,18 @@ import { ProductRepository } from "../model/product.repository";
 export class StoreComponent {
   constructor(private repository: ProductRepository) { }
 
+  public selectedCategory = null;
+
   get products(): Product[] {
-    return this.repository.getProducts();
+    return this.repository.getProducts(this.selectedCategory);
   }
   get categories(): string[] {
     return this.repository.getCategories();
   }
+
+  changeCategory(newCategory?: string) {
+    this.selectedCategory = newCategory;
+  }
+
 }
 
