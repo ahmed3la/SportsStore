a4aaa3e58176c5e94586b134bf90c09d90deb76a
ahmed <ahmed3la@gmail.com>
2021-03-29 00:00:06 +0200

    Creating the Cart Summary Components

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Cart } from "../model/cart.model";
 import { Product } from "../model/product.model";
 import { ProductRepository } from "../model/product.repository";
 
@@ -8,7 +9,10 @@ import { ProductRepository } from "../model/product.repository";
   templateUrl: "store.component.html"
 })
 export class StoreComponent {
-  constructor(private repository: ProductRepository) { }
+  constructor(private repository: ProductRepository,
+    private cart: Cart) {
+
+  }
 
   public selectedCategory = null;
   public productsPerPage = 4;
@@ -40,6 +44,10 @@ export class StoreComponent {
   get pageCount(): number {
     return Math.ceil(this.repository
       .getProducts(this.selectedCategory).length / this.productsPerPage)
+  }
+
+  addProductToCart(product: Product) {
+    this.cart.addLine(product);
   }
 
   // get pageNumbers(): number[] {
